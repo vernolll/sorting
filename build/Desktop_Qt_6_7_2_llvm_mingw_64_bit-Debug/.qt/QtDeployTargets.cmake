c87cb3c03eeafc9c1709a78b5fa0eb40f5789c66
vernolll <marina2006.karas@yandex.ru>
2024-10-03 09:52:55 +0300

    init

---
+++ build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_laba1_algos_FILE D:/university/3 semestr/laba1_algos/build/Desktop_Qt_6_7_2_llvm_mingw_64_bit-Debug/laba1_algos.exe)
+set(__QT_DEPLOY_TARGET_laba1_algos_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_laba1_algos_RUNTIME_DLLS C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/llvm-mingw_64/bin/Qt6Core.dll)
